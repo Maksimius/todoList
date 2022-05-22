f090b3b786758be3027580295d745356ab2b1594
Maks H <maksim.hatin@neoseo.com.ua>
2022-05-22 19:47:57 +0300

    midration+controllers

---
+++ database/factories/DoingsFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Doings>
+ */
+class DoingsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'finished' => $this->faker->randomNumber([0,1]),
+            'urgent' => $this->faker->randomNumber([0,1]),
+        ];
+    }
+}
