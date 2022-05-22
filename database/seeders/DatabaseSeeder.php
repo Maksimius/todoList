02db33326349113d334ef94d7d0f014fc37ce35b
Maks H <maksim.hatin@neoseo.com.ua>
2022-05-22 19:44:50 +0300

    midration+controllers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Doings;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -20,5 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Doings::factory(10)->create();
     }
 }
